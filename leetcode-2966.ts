41c7d73954de869ae66cde1d42fc50bcf9f6bfc8
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2024-02-02 23:56:40 +0100

    Leetcode challenge for February 1st 2024

--- leetcode-2966.ts
+++ leetcode-2966.ts
@@ -1,11 +1,9 @@
 function divideArray(nums: number[], k: number): number[][] {
     nums.sort((a, b) => a - b)
     const answer: number[][] = []
-
     for (let i = 0; i < nums.length; i+=3) {
         if (Math.abs(nums[i] - nums[i + 2]) > k)  return []
         answer.push([nums[i], nums[i + 1], nums[i + 2]])
     }
-
     return answer
 };
