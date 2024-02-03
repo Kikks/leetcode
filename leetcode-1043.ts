1e7c6e4b4ba78e1ef4e4a18898c92f7ab318edfe
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2024-02-03 23:55:42 +0100

    Leetcode challenge for February 3rd 2024

---
+++ leetcode-1043.ts
@@ -0,0 +1,18 @@
+function maxSumAfterPartitioning(arr: number[], k: number): number {
+	let n = arr.length;
+	let dp = new Array(n + 1).fill(0);
+
+	for (let i = 1; i <= n; i++) {
+		let maxElement = 0;
+		let maxSum = 0;
+
+		for (let j = 1; j <= k && i - j >= 0; j++) {
+			maxElement = Math.max(maxElement, arr[i - j]);
+			maxSum = Math.max(maxSum, dp[i - j] + maxElement * j);
+		}
+
+		dp[i] = maxSum;
+	}
+
+	return dp[n];
+}
