08654bb8254ae62543eca9a1d8403cf979a7c760
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2024-02-14 23:53:24 +0100

    Leetcode challenge for February 13th 2024

--- leetcode-2108.ts
+++ leetcode-2108.ts
@@ -1,8 +1,6 @@
 function isPalindrome(s: string) {
 	for (let i = 0; i < Math.floor(s.length / 2); i++) {
-		if (s[i] !== s[s.length - i - 1]) {
-			return false;
-		}
+		if (s[i] !== s[s.length - i - 1]) return false;
 	}
 	return true;
 }
