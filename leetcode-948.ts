e470ad628bfd45616cafa704d4e96b28d71f020a
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2024-03-04 23:42:55 +0100

    Leetcode challenge for March 4th 2024

--- leetcode-948.ts
+++ leetcode-948.ts
@@ -1,50 +1,22 @@
 function bagOfTokensScore(tokens: number[], power: number): number {
-	if (!tokens || tokens.length === 0) {
-		return 0;
-	}
-
-	quicksort(tokens, 0, tokens.length - 1);
-	let left = 0,
-		score = 0,
-		maxScore = 0,
-		right = tokens.length - 1;
-
-	while (left <= right) {
-		if (power >= tokens[left]) {
-			power -= tokens[left++];
+	tokens.sort((a, b) => a - b);
+	let score = 0;
+	let maxScore = 0;
+	let i = 0;
+	let j = tokens.length - 1;
+	while (i <= j) {
+		if (power >= tokens[i]) {
+			power -= tokens[i];
 			score++;
+			i++;
+			maxScore = Math.max(maxScore, score);
 		} else if (score > 0) {
-			power += tokens[right--];
+			power += tokens[j];
 			score--;
+			j--;
 		} else {
 			break;
 		}
-
-		maxScore = Math.max(score, maxScore);
 	}
-
 	return maxScore;
-}
-
-function quicksort(arr: number[], left: number, right: number): void {
-	if (left < right) {
-		const pivotIndex = partition(arr, left, right);
-		quicksort(arr, left, pivotIndex - 1);
-		quicksort(arr, pivotIndex + 1, right);
-	}
-}
-
-function partition(arr: number[], left: number, right: number): number {
-	const pivotValue = arr[right];
-	let i = left - 1;
-
-	for (let j = left; j < right; j++) {
-		if (arr[j] < pivotValue) {
-			i++;
-			[arr[i], arr[j]] = [arr[j], arr[i]];
-		}
-	}
-
-	[arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
-	return i + 1;
 }
